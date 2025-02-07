c65281f5674cc4902df92aa5498c800eb8a13cd6
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-02-07 18:45:51 +0000

    Added assembly info for media bar plugin

---
+++ src/Jellyfin.Plugin.MediaBar/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+[assembly: AssemblyCompany("IAmParadox27")]
+[assembly: AssemblyProduct("Jellyfin.Plugins.MediaBar")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Jellyfin.Plugins.MediaBar")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
