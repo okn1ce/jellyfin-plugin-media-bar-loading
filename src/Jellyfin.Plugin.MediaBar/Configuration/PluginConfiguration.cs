44c4c7e3a6c1b6e108941ecaba0207638b7f35cb
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-02-24 22:10:51 +0000

    Added configuration page to enable and disable the plugin. Allows for future proofing in case there are optional configurations (like the normal mode before support for it was dropped)

---
+++ src/Jellyfin.Plugin.MediaBar/Configuration/PluginConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin.Plugin.MediaBar.Configuration
+{
+    public enum MediaBarState
+    {
+        Disabled,
+        Enabled,
+    }
+    
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+        public MediaBarState Enabled { get; set; } = MediaBarState.Enabled;
+    }
+}
--- src/Jellyfin.Plugin.MediaBar/MediaBarPlugin.cs
+++ src/Jellyfin.Plugin.MediaBar/MediaBarPlugin.cs
@@ -1,11 +1,12 @@
-﻿using MediaBrowser.Common.Configuration;
+﻿using Jellyfin.Plugin.MediaBar.Configuration;
+using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
 namespace Jellyfin.Plugin.MediaBar
 {
-    public class MediaBarPlugin : BasePlugin<BasePluginConfiguration>
+    public class MediaBarPlugin : BasePlugin<PluginConfiguration>
     {
         public override Guid Id => Guid.Parse("08f615ea-2107-4f04-89cc-091035f54448");
 
